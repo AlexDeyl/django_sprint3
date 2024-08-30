32ef5c596737d54ce5ad4b0118eeb224ba1ad727
AlexDeyl <147546536+AlexDeyl@users.noreply.github.com>
2024-08-30 12:54:21 +0300

    Add files via upload

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -10,30 +10,31 @@ utc_now = datetime.now(timezone.utc)
 
 def index(request: HttpRequest) -> HttpResponse:
     """Главная страница"""
-    template = 'blog/index.html'
+    template = "blog/index.html"
     now = timezone.now()
+    # Выводим на главную стрницу 5 последих постов
     post_list = Post.objects.filter(
-        pub_date__lte=now,
-        is_published=True,
-        category__is_published=True
-    ).order_by('-pub_date')[:5]
-    context = {'post_list': post_list}
+        pub_date__lte=now, is_published=True, category__is_published=True
+    ).order_by("-pub_date")[:5]
+    context = {"post_list": post_list}
     return render(request, template, context)
 
 
 def post_detail(request: HttpRequest, pk) -> HttpResponse:
     """Детали поста"""
-    template = 'blog/detail.html'
+    template = "blog/detail.html"
     post = get_object_or_404(Post, pk=pk, is_published=True)
     if post.pub_date > timezone.now() or not post.is_published:
         raise Http404("Пост не опубликован или устарел")
-    context = {'post': post}
+    if not post.category.is_published:
+        raise Http404("Категория снята с публикации")
+    context = {"post": post}
     return render(request, template, context)
 
 
 def category_posts(request: HttpRequest, category_slug) -> HttpResponse:
     """Страница по категориям"""
-    template = 'blog/category.html'
+    template = "blog/category.html"
     try:
         category = Category.objects.get(slug=category_slug, is_published=True)
     except Category.DoesNotExist:
@@ -41,6 +42,10 @@ def category_posts(request: HttpRequest, category_slug) -> HttpResponse:
     # Получаем все посты, связанные с данной категорией
     posts = category.post_set.all()
     # Фильтруем посты, чтобы отображать только опубликованные
-    post_list = list(filter(lambda post: post.is_published, posts))
-    context = {'category': category, 'post_list': post_list}
+    post_list = list(
+        filter(
+            lambda post: post.is_published and not post.pub_date > timezone.now(), posts
+        )
+    )
+    context = {"category": category, "post_list": post_list}
     return render(request, template, context)
