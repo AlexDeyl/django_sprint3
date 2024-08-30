d5a4510616d77e6a24b5deb0e5558b90d8b89431
AlexDeyl <147546536+AlexDeyl@users.noreply.github.com>
2024-08-30 13:05:41 +0300

    Delete blogicum directory

--- blogicum/pages/urls.py
+++
@@ -1,9 +0,0 @@
-from .views import about, rules
-from django.urls import path
-
-app_name = 'pages'
-
-urlpatterns = [
-    path('about/', about, name='about'),
-    path('rules/', rules, name='rules'),
-]
